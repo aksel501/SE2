9aab53c53396103e069b2eb4e79e415c8de25381
DESKTOP-IRPGC5I\aksel <aksel501@yandex.ru>
2017-02-24 08:58:24 -0500

    Expert register

--- MUE/MUE/Controllers/AccountController.cs
+++ MUE/MUE/Controllers/AccountController.cs
@@ -22,7 +22,7 @@ namespace MUE.Controllers
         {
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -34,9 +34,9 @@ namespace MUE.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -120,7 +120,7 @@ namespace MUE.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -141,30 +141,83 @@ namespace MUE.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        //added by nate
+        public ActionResult ExpertRegistration()
+        {
+
+            return View();
+        }
 
         //
         // POST: /Account/Register
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> Register(AccountRegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser {
-
-                    UserName = model.Email,
-
-                    Email = model.Email
-
-
-
-                };
+                //string EncodedResponse = Request.Form["g-Recaptcha-Response"];
+                //bool IsCaptchaValid=(ReCaptcha.Validate(EncodedResponse)=="True" ? true: false);
+                //if (IsCaptchaValid)
+                //{
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
+
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.MiddleInit = model.MiddleInt;
+
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+                //}
+                //else {
+
+                //    TempData["recaptcha"] = "Please verify that you are not a robot";
+
+                //}
+                //AccountRegisterViewModel ARVM = new AccountRegisterViewModel();
+
+
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+        //for registering an expert account
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        //added by nate
+        public async Task<ActionResult> ExpertRegistration(AccountRegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //AccountRegisterViewModel ARVM = new AccountRegisterViewModel();
+
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                //var expert = new Expert { Specialty = model.Specialty };
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.MiddleInit = model.MiddleInt;
+
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
