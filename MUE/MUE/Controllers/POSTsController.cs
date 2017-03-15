df53ec0d194c3576f7cf212c9fba86fd9500e476
HenokH <henokats@gmail.com>
2017-03-15 00:39:45 -0400

    final Change

---
+++ MUE/MUE/Controllers/POSTsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MUE.Models;
+
+namespace MUE.Controllers
+{
+    [Authorize]
+    public class POSTsController : Controller
+    {
+        private ModelReferencesHere db = new ModelReferencesHere();
+
+        // GET: POSTs
+        public ActionResult Index()
+        {
+            return View();
+            // var pOSTs = db.POSTs.Include(p => p.AspNetUser);
+            //return View(pOSTs.ToList());
+        }
+
+        public ActionResult IndexList()
+        {
+            return View();
+        } 
+        // GET: POSTs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            POST pOST = db.POSTs.Find(id);
+            if (pOST == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pOST);
+        }
+
+        // GET: POSTs/Create
+        public ActionResult Create()
+        {
+            ViewBag.USERID = new SelectList(db.AspNetUsers, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: POSTs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,USERID,SUBJECT,TITLE,DATETIMEPOSTED")] POST pOST)
+        {
+            if (ModelState.IsValid)
+            {
+                db.POSTs.Add(pOST);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.USERID = new SelectList(db.AspNetUsers, "Id", "FirstName", pOST.USERID);
+            return View(pOST);
+        }
+
+        // GET: POSTs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            POST pOST = db.POSTs.Find(id);
+            if (pOST == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.USERID = new SelectList(db.AspNetUsers, "Id", "FirstName", pOST.USERID);
+            return View(pOST);
+        }
+
+        // POST: POSTs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,USERID,SUBJECT,TITLE,DATETIMEPOSTED")] POST pOST)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pOST).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.USERID = new SelectList(db.AspNetUsers, "Id", "FirstName", pOST.USERID);
+            return View(pOST);
+        }
+
+        // GET: POSTs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            POST pOST = db.POSTs.Find(id);
+            if (pOST == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pOST);
+        }
+
+        // POST: POSTs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            POST pOST = db.POSTs.Find(id);
+            db.POSTs.Remove(pOST);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
