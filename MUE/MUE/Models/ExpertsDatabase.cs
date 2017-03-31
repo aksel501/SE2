80d5d64feab8c19162435dce1b154512ea0c1f4b
Damian Adams <DamianAdams96@hotmail.com>
2017-03-31 13:26:53 -0400

    up

--- MUE/MUE/Models/ExpertsDatabase.cs
+++ MUE/MUE/Models/ExpertsDatabase.cs
@@ -13,6 +13,7 @@ namespace MUE.Models
         }
 
         public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
+
         public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
         public virtual DbSet<AspNetUserLogin> AspNetUserLogins { get; set; }
         public virtual DbSet<AspNetUser> AspNetUsers { get; set; }
